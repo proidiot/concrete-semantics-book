15d4dfd41e3bf16207a56eb3209820fb453931fe
Charles Southerland <proidiot@gmail.com>
2022-06-20 18:49:58 -0500

    Exercise 5.4 clarification

--- ch5/Exercise4.thy
+++ ch5/Exercise4.thy
@@ -6,13 +6,11 @@ inductive ev :: "nat \<Rightarrow> bool" where
 ev0: "ev 0" |
 evSS: "ev n \<Longrightarrow> ev (Suc (Suc n))"
 
-lemma "\<not> ev (Suc (Suc (Suc 0)))" (is "\<not> ?E3")
+lemma "\<not> ev (Suc (Suc (Suc 0)))" (is "\<not> ?EVEN3")
 proof
-  assume "?E3"
-  thus False
-  proof cases
-    case evSS thus False using `?E3` ev.cases by auto
-  qed
+  assume "?EVEN3"
+  hence "ev (Suc 0)" by cases
+  thus False by cases
 qed
 
 end
