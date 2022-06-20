96185af2923823e8a87f7adc075c23438848f355
Charles Southerland <proidiot@gmail.com>
2022-06-20 14:59:04 -0500

    Exercise 5.1

---
+++ ch5/Exercise1.thy
@@ -0,0 +1,18 @@
+theory Exercise1
+  imports Main
+begin
+
+lemma assumes T: "\<forall> x y. T x y \<or> T y x"
+  and A: "\<forall> x y. A x y \<and> A y x \<longrightarrow> x = y"
+  and TA: "\<forall> x y. T x y \<longrightarrow> A x y" and "A x y"
+shows "T x y"
+proof (rule ccontr)
+  assume "\<not> T x y"
+  hence "T y x" using T by auto
+  hence "A y x" using TA by simp
+  hence "x = y" using A and `A x y` by simp
+  hence "T x y" using `T y x` by blast
+  thus "False" by (simp add: `\<not> T x y`)
+qed
+
+end
